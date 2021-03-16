d3b3c2f7da1135211e5a2ac658207a129dd88973
Chetan Aggarwal <Chetan.Aggarwal@amdocs.com>
2021-03-16 18:09:10 +0530

    basic movies-app-get-api

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
-const developersObj = require('./backend/src/developers');
+const router = express.Router();
+const { default: axios } = require('axios');
 
 // To define the port
-const port = process.env.PORT || 3100;
+const port = process.env.PORT || 4000;
 
 // To work on the response in json
-app.use(express.json());
+router.use(express.json());
 
-// To work on developers
-app.use(developersObj);
+movies = {};
+const getMovies = (req, res) => {
+    const DEFAULT_MOVIES_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=61cec280feda32b806f9f3187c008707&page=1";
+    axios(DEFAULT_MOVIES_API)
+    .then(res => res.data)
+    .then(data => {
+        movies = data;
+        res.send(movies);
+    })
+};
+
+router.get('/', getMovies);
 
 app.listen(port, () => {
     console.log(`Server listening at port: ${port}`);
